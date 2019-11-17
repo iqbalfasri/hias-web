4c4c1ef723db5039d2bcacec74c549a6a3690ed8
Iqbal Fasri <iqbalx64@gmail.com>
2019-11-17 21:54:44 +0700

    create simple accordion with css

--- src/pages/Articles/Promo.js
+++ src/pages/Articles/Promo.js
@@ -3,6 +3,23 @@ import React, { Component } from "react";
 import "./Promo.scss";
 
 class Promo extends Component {
+  componentDidMount() {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    }
+  }
+
   render() {
     return (
       <>
@@ -23,7 +40,35 @@ class Promo extends Component {
         </div>
 
         <div className="promo-content">
-          <h1>CONTENT</h1>
+          <div className="container">
+            <h1>CONTENT</h1>
+          </div>
+        </div>
+
+        <div className="container" style={{ marginBottom: 250 }}>
+          <h1>example dropdown</h1>
+
+          <div>
+            <button class="accordion">Section 1</button>
+            <div class="panel">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+
+            <button class="accordion">Section 2</button>
+            <div class="panel">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </div>
+          </div>
         </div>
       </>
     );
