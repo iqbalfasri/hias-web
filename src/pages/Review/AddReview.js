58965a15dca2aaf3ce8ce7d8e3d850cc61b93f60
agsen <agungsn1994@gmail.com>
2019-11-20 09:30:17 +0700

    update review

--- src/pages/Review/AddReview.js
+++ src/pages/Review/AddReview.js
@@ -4,7 +4,9 @@ import { fetchProductByInspirationId, fetchWishList } from '../../api'
 import { withContext } from '../../context/withContext'
 import { isLogin } from '../../utils/auth'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons'
+import axios from "axios"
 
 import InputText from '../../components/form/InputText'
 import "./addReview.scss";
@@ -15,7 +17,9 @@ class AddReview extends Component {
 
         this.state = {
             rating: 0,
-            reviewText: ""
+            reviewText: "",
+            isLoading: false,
+            isSentSucces: ""
         }
     }
 
@@ -23,20 +27,101 @@ class AddReview extends Component {
 
     }
 
+    onChangeRating = (i) => {
+        this.setState({
+            rating: i
+        })
+    }
+    onChangeReviewText = (e) => {
+        this.setState({
+            reviewText: e.target.value
+        })
+    }
+
     renderRating = () => {
+        const { rating } = this.state;
+
         return (
             <div>
-                <FontAwesomeIcon icon={faStar} />
-                <FontAwesomeIcon icon={faStar} />
-                <FontAwesomeIcon icon={faStar} />
-                <FontAwesomeIcon icon={faStar} />
-                <FontAwesomeIcon icon={faStar} />
+                <FontAwesomeIcon icon={rating < 1 ? farStar : fasStar} onClick={() => { this.onChangeRating(1) }} />
+                <FontAwesomeIcon icon={rating < 2 ? farStar : fasStar} onClick={() => { this.onChangeRating(2) }} />
+                <FontAwesomeIcon icon={rating < 3 ? farStar : fasStar} onClick={() => { this.onChangeRating(3) }} />
+                <FontAwesomeIcon icon={rating < 4 ? farStar : fasStar} onClick={() => { this.onChangeRating(4) }} />
+                <FontAwesomeIcon icon={rating < 5 ? farStar : fasStar} onClick={() => { this.onChangeRating(5) }} />
             </div>
         )
     }
 
+    sendMessage1 = () => {
+        const { rating, reviewText } = this.state;
+        const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+        const userId = parseInt(localStorage.getItem('userId'), 10);
+        const valueDiscussion = {
+            userId: userId,
+            productId: 2,
+            discussion: reviewText
+        }
+        const valueRating = {
+            productId: 2,
+            userId: userId,
+            rating: rating
+        }
+        //
+
+        this.setState({ isLoading: true })
+        axios
+            .post(`${BASE_URL}/product/discussion`, valueDiscussion, { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } })
+            .then((res) => {
+                console.log('berhasil')
+            })
+            .catch(() => {
+                this.setState({ isSentSuccess: "no", isLoading: false })
+            })
+    }
+
+    sendMessage = () => {
+        const { rating, reviewText } = this.state;
+        const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+        const userId = parseInt(localStorage.getItem('userId'), 10);
+        const valueDiscussion = {
+            userId: userId,
+            productId: 2,
+            discussion: reviewText
+        }
+        const valueRating = {
+            productId: 2,
+            userId: userId,
+            rating: rating
+        }
+        //
+
+        this.setState({ isLoading: true })
+        axios
+            .post(`${BASE_URL}/product/discussion`, valueDiscussion, { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } })
+            .then((res) => {
+                axios
+                    .post(`${BASE_URL}/product/discussion`, valueRating, { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } })
+                    .then((res) => {
+                        this.setState({ isSentSuccess: "yes" })
+                    })
+                    .catch(() => {
+                        this.setState({ isSentSuccess: "no" })
+                    })
+                    .finally(() => {
+                        this.setState({ isLoading: false })
+                    })
+
+            })
+            .catch(() => {
+                this.setState({ isSentSuccess: "no", isLoading: false })
+            })
+    }
+
 
     render() {
+        console.log(this.state)
         return (
             <div>
                 <div className="content">
@@ -64,7 +149,7 @@ class AddReview extends Component {
                                     <div className="col">
                                         <div className="form--group">
                                             <label htmlFor="">Review</label>
-                                            <textarea className="text--area" type="text" placeholder="Tuliskan Review" value={this.state.message} onChange={this.onChangeMessage} />
+                                            <textarea className="text--area" type="text" placeholder="Tuliskan Review" value={this.state.reviewText} onChange={this.onChangeReviewText} />
                                         </div>
                                     </div>
                                 </div>
@@ -72,8 +157,19 @@ class AddReview extends Component {
                                     <button
                                         onClick={this.sendMessage}
                                         className="btn btn--full btn--blue"
-                                        style={{ maxWidth: "100%" }}>Submit</button>
+                                        style={{ maxWidth: "100%" }}>{this.state.isLoading ? "Mengirim..." : "Submit"}</button>
                                 </div>
+
+                                {this.state.isSentSuccess === "yes" &&
+                                    <div className="col">
+                                        <p />
+                                        <p>Review anda berhasil disimpan</p>
+                                    </div>}
+                                {this.state.isSentSuccess === "no" &&
+                                    <div className="col">
+                                        <p />
+                                        <p>Review anda gagal disimpan. </p>
+                                    </div>}
                             </div>
                         </div>
                     </section>
--- src/pages/Review/Review.js
+++ src/pages/Review/Review.js
@@ -1,43 +1,71 @@
 import React, { Component } from 'react'
-
-import { fetchProductByInspirationId, fetchWishList } from '../../api'
-import { withContext } from '../../context/withContext'
-import { isLogin } from '../../utils/auth'
+import axios from 'axios'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import './Review.scss'
 
 class Review extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            inspiration: null,
-            wishListItems: []
-        }
+    state = {
+        review: []
     }
 
     componentDidMount() {
+        const BASE_URL = process.env.REACT_APP_BASE_URL;
+        axios.get(`${BASE_URL}/product/discussion/2`, { headers: { Authorization: `Bearer ${localStorage.getItem('token')}` } })
+            .then((res) => {
+                console.log(res)
+                if (res.data.length !== 0) {
+                    this.setState({
+                        review: res.data.data
+                    })
+                }
+            })
+    }
+
+    renderReview = () => {
+        const { review } = this.state;
+        if (review !== null) {
+            return review.map((item, i) => {
+                return (
+                    <div class="review-item" key={i}>
+                        <div className="icon">
+                            <FontAwesomeIcon icon={faUserCircle} style={{ height: "24px" }} />
+                        </div>
+                        <div>
+                            <h3>{item.user.fullName}</h3>
+                            <p >{item.discussion !== null ? item.discussion : "-"}</p>
+                        </div>
+                    </div>
+                )
+            })
+        }
 
     }
+
 
 
     render() {
-        const { inspiration } = this.state;
+        console.log(this.state)
         return (
-            <div>
-                <div className="content">
-                    <section className="section-page">
-                        <div className="container">
-                            <div className="row align-items-center mb--2">
+            <div className="content">
+                <div className="section-page">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-4">
                                 <div>
-                                    <h3 className="section-title mb--0">Reviews</h3>
+                                    <h2>Review</h2>
+                                    <p>Testimoni customer yang telah belanja di hias house</p>
                                 </div>
                             </div>
-                            <div className="row">
-                            </div>
                         </div>
-                    </section>
+                        <div className="contianer-review-list" >
+                            {this.renderReview()}
+                        </div>
+                    </div>
                 </div>
-            </div>)
+            </div>
+        )
     }
 }
 
-export default withContext(Review)
+export default Review
