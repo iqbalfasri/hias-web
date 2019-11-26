5df513f034ec332cc7a449d7931a58196307cea0
Iqbal Fasri <iqbalx64@gmail.com>
2019-11-26 09:44:03 +0700

    sub total to global state

--- src/context/Context.js
+++ src/context/Context.js
@@ -20,7 +20,9 @@ export const Context = createContext(
       isModalPromo: false,
       setIsModalPromo: () => { },
       totalCart: 0,
-      setTotalCart: () => { }
+      setTotalCart: () => { },
+      subTotal: null,
+      setSubTotal: () => { }
     }
   }
 )
