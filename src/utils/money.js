12e5211c92d5ee7552ebba59fbf832ac964d3a6e
Iqbal Fasri <iqbalx64@gmail.com>
2019-12-05 02:45:44 +0700

    fix discount calculate

--- src/utils/money.js
+++ src/utils/money.js
@@ -1,3 +1,12 @@
-export const formatMoneyWithoutSymbol = (money) => {
-  return Math.abs(money).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1.')
-}
+export const formatMoneyWithoutSymbol = money => {
+  return Math.abs(money)
+    .toString()
+    .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.");
+};
+
+export const getDiscount = (price, discount) => {
+  let disc = price * (discount / 100);
+  let net = price - disc;
+
+  return parseInt(net);
+};
